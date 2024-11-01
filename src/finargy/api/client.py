658092846d1b52d4706ee0bb8700258531d16337
Santiago Elewaut <santielewaut@gmail.com>
2024-11-01 00:11:35 -0300

    Format file

--- src/finargy/api/client.py
+++ src/finargy/api/client.py
@@ -6,15 +6,15 @@ from dotenv import load_dotenv
 import os
 
 
-BASE_URL = 'https://api.invertironline.com/api/v2'
-TOKEN_URL = 'https://api.invertironline.com/token'
+BASE_URL = "https://api.invertironline.com/api/v2"
+TOKEN_URL = "https://api.invertironline.com/token"
 
 
 class InvertirOnlineAPI:
     def __init__(self, base_url=BASE_URL, token_url=TOKEN_URL):
         # load environment variables
         load_dotenv()
-        self.username = os.getenv("USERNAME")
+        self.username = os.getenv("USER")
         self.password = os.getenv("PASSWORD")
         self.base_url = base_url
         self.token_url = token_url
@@ -26,18 +26,20 @@ class InvertirOnlineAPI:
     def _authenticate(self):
         """Authenticate and obtain access token."""
         data = {
-            'grant_type': 'password',
-            'username': self.username,
-            'password': self.password
+            "grant_type": "password",
+            "username": self.username,
+            "password": self.password,
         }
         response = requests.post(self.token_url, data=data)
         response.raise_for_status()
         # get authentication token
         token_info = response.json()
-        self.access_token = token_info['access_token']
-        expires_in = token_info['expires_in']
-        self.token_expiry = datetime.datetime.now() + datetime.timedelta(seconds=expires_in)
-        print(f'Authenticated. Token expires at {self.token_expiry}')
+        self.access_token = token_info["access_token"]
+        expires_in = token_info["expires_in"]
+        self.token_expiry = datetime.datetime.now() + datetime.timedelta(
+            seconds=expires_in
+        )
+        print(f"Authenticated. Token expires at {self.token_expiry}")
 
     def _check_token_expiry(self):
         """Check if the token has expired and re-authenticate if necessary."""
@@ -47,12 +49,12 @@ class InvertirOnlineAPI:
     def _request(self, method, endpoint, params=None, data=None):
         """Make an API request."""
         self._check_token_expiry()
-        url = f'{self.base_url}/{endpoint}'
+        url = f"{self.base_url}/{endpoint}"
         headers = {
-            'Authorization': f'Bearer {self.access_token}',
+            "Authorization": f"Bearer {self.access_token}",
         }
-        response = requests.request(method, url, headers=headers, params=params, json=data)
+        response = requests.request(
+            method, url, headers=headers, params=params, json=data
+        )
         response.raise_for_status()
         return response.json()
-
-
